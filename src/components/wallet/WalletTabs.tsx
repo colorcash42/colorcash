bb3f5dd459388f03961af914e6ce3894b98590ae
Rotiyana Foods <rotiyanafoods@gmail.com>
2025-08-08 11:43:15 +0000

    UI/UX design professional karo

--- src/components/wallet/WalletTabs.tsx
+++ src/components/wallet/WalletTabs.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 import { useState } from 'react';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
@@ -319,5 +320,3 @@ export function WalletTabs() {
     </Tabs>
   );
 }
-
-    
