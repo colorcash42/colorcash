ee196eaae87368df71dcfa9852dbb59c44a874a9
Rotiyana Foods <rotiyanafoods@gmail.com>
2025-08-09 13:44:33 +0000

    Ab kya kare or

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  // This is the crucial change to disable image optimization
-  // and allow the build to succeed with static images in the public folder.
-  images: {
-    unoptimized: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
